a920eb77b03c76c7c4eccdfaf94bae0beed95fe5
Jordan Elizaga <elizaga.jn@gmail.com>
2017-09-20 02:36:30 -0500

    Can use a read-only database, trying to implement a system to a new post can get added, so like a one-to-many relationship

--- projects/admin.py
+++ projects/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Project, Description, BulletinPost
+
+admin.site.register(Project)
+admin.site.register(Description)
+admin.site.register(BulletinPost)
--- projects/views.py
+++ projects/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import Project 
+
 def index(request):
+    project_list = Project.objects.order_by('-pub_date')[:]
     # Render the HTML template index.html with the data in the context variable
     return render(
         request,
         'projects.html',
-        context={},
+        context={ 'project_list': project_list, },
     )
