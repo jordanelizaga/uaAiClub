736eebdf8d6fbde9a56c0ffb2ab3f9d6b987d397
Jordan Elizaga <elizaga.jn@gmail.com>
2017-09-21 14:49:01 -0500

    trying to fix this error
      File ".\projects\views.py", line 23, in index
          form = PostForm(initial={ 'post_date': datetime.now(), },)

--- projects/views.py
+++ projects/views.py
@@ -9,7 +9,7 @@ def index(request):
     # if this is a POST request we need to process the form data
     if request.method == 'POST':
         # create a form instance and populate it with data from the request:
-        form = PostForm(request.POST,)
+        form = PostForm(request.POST, initial={ 'post_date': datetime.now(), },)
         # check whether it's valid:
         if form.is_valid():
             # process the data in form.cleaned_data as required
@@ -20,7 +20,8 @@ def index(request):
 
     # if a GET (or any other method) we'll create a blank form
     else:
-        form = PostForm(initial={ 'post_date': datetime.now(), },)
+        # form = PostForm(initial={ 'post_date': datetime.now(), },)
+        form = PostForm()
 
     project_list = Project.objects.order_by('-pub_date')[:]
     # Render the HTML template index.html with the data in the context variable
