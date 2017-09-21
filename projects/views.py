2759b3834dfdc442ed7e95e321b9d9b7dc7d5569
Jordan Elizaga <elizaga.jn@gmail.com>
2017-09-21 09:02:02 -0500

    SUCCESSFULLY MADE IT IN TIME

--- projects/views.py
+++ projects/views.py
@@ -9,18 +9,18 @@ def index(request):
     # if this is a POST request we need to process the form data
     if request.method == 'POST':
         # create a form instance and populate it with data from the request:
-        form = PostForm(request.POST, initial={ 'post_date': datetime.now(), },)
+        form = PostForm(request.POST,)
         # check whether it's valid:
         if form.is_valid():
             # process the data in form.cleaned_data as required
             # ...
             # redirect to a new URL:
             form.save()
-            return HttpResponseRedirect('/thanks/')
+            return HttpResponseRedirect('/projects/')
 
     # if a GET (or any other method) we'll create a blank form
     else:
-        form = PostForm()
+        form = PostForm(initial={ 'post_date': datetime.now(), },)
 
     project_list = Project.objects.order_by('-pub_date')[:]
     # Render the HTML template index.html with the data in the context variable
