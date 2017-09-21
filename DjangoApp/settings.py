376156ff5b9ae0a1aa1de6cbddaa1126b80aafe2
Jordan Elizaga <elizaga.jn@gmail.com>
2017-09-21 14:40:50 -0500

    Added TEMPLATES to settings.py, to meet satisfy log files:

--- DjangoApp/settings.py
+++ DjangoApp/settings.py
@@ -101,6 +101,25 @@ TEMPLATE_LOADERS = (
 #     'django.template.loaders.eggs.Loader',
 )
 
+# added templates from here https://docs.djangoproject.com/en/1.11/ref/templates/upgrading/#the-templates-settings
+# azure log file said so
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+
 MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
