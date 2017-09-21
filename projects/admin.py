91388695092dc9edbe5b81a45ef6729e1b972a75
Jordan Elizaga <elizaga.jn@gmail.com>
2017-09-21 09:12:10 -0500

    EDIT: removed admin priv, Copied and pasted to deploy, hopefully it works

--- projects/admin.py
+++ projects/admin.py
@@ -3,6 +3,6 @@ from django.contrib import admin
 # Register your models here.
 from .models import Project, Description, BulletinPost
 
-admin.site.register(Project)
-admin.site.register(Description)
-admin.site.register(BulletinPost)
+# admin.site.register(Project)
+# admin.site.register(Description)
+# admin.site.register(BulletinPost)
