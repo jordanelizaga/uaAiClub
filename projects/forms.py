109744a18ad3ba51e7545d41628c40515bfd4d54
Jordan Elizaga <elizaga.jn@gmail.com>
2017-09-27 23:12:27 -0500

    Fixed bulletin post form to textarea

--- projects/forms.py
+++ projects/forms.py
@@ -8,7 +8,11 @@ class PostForm(forms.ModelForm):
     class Meta: 
         model = BulletinPost
         fields = ['project', 'post_date', 'post_user', 'post_message' ]
-        widgets = {'project': forms.HiddenInput(), 'post_date': forms.HiddenInput()}
+		widgets = {
+            'project': forms.HiddenInput(), 
+            'post_date': forms.HiddenInput(),
+            'post_message' :  forms.Textarea(attrs={'cols': 55, 'rows': 20}),
+        }
 
 class ProjectForm(forms.ModelForm):
     class Meta:
