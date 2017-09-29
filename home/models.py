c7b40691990cd771f8835cc1c33555d05249c6eb
Jordan Elizaga <elizaga.jn@gmail.com>
2017-09-28 23:21:12 -0500

    Added newsletter functionality

--- home/models.py
+++ home/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
+from django.forms import ModelForm
+
 # Create your models here.
+# e-mail notification model
+class Newsletter(models.Model):
+    email = models.EmailField(max_length=254)
+    firstName = models.CharField(max_length=100)
+    lastName = models.CharField(max_length=100)
+    def __str__(self):
+        return self.email
+
+class NewsletterForm(ModelForm):
+    class Meta:
+        model = Newsletter
+        labels = { 
+            "email" : "E-Mail",
+            "firstName" : "First Name",
+            "lastName" : "Last Name",
+        }
+        fields = '__all__'
