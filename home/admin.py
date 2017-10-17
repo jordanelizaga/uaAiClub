b8e10461a7117eb0eeb4f521e8ab5f613109e584
Jordan Elizaga <elizaga.jn@gmail.com>
2017-10-16 23:56:06 -0500

    Working editable schedule

--- home/admin.py
+++ home/admin.py
@@ -2,5 +2,7 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import Newsletter
+from .models import Schedule
 
 admin.site.register(Newsletter)
+admin.site.register(Schedule)
