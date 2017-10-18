fd235508149551799ea44ff06a882e942c040db9
Jordan Elizaga <elizaga.jn@gmail.com>
2017-10-18 01:07:27 -0500

    Fixed event page

--- events/views.py
+++ events/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import Event
+
 def index(request):
+    event_list = Event.objects.order_by('-date')
+    context = { 'event_list': event_list, }
     # Render the HTML template about.html with the data in the context variable
     return render(
         request,
         'events.html',
-        context={},
+        context,
     )
